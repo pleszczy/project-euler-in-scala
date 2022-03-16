0d4c4756f442ec7c33e9c4d69723c1e919759ccf
Piotr <piotrkun@gmail.com>
2022-03-16 07:56:04 +0100

    Initial commit

---
+++ src/main/scala/com/piotr/euler/Task1.scala
@@ -0,0 +1,12 @@
+package com.piotr.euler
+
+object Task1 {
+
+  def main(args: Array[String]): Unit = {
+    val sumOfMultiples = (1 to 1000).collect { case x if x % 3 == 0 || x % 5 == 0 => x }.sum
+    println(sumOfMultiples)
+  }
+
+
+
+}
